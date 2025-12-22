cb4def92f75acf5ef53bc741db7734ac55ec00d0
ROHIT KUMAR <21mc3013@rgipt.ac.in>
2025-12-22 15:08:28 +0530

    Add files via upload

---
+++ static/js/dashboard.js
@@ -0,0 +1,22 @@
+const expanderBtn = document.getElementById('expander-btn');
+
+expanderBtn.addEventListener('click', () => {
+    const navigationInner = document.querySelector('.navigation-inner');
+
+    if (navigationInner.classList.contains('hidden')) {
+        navigationInner.classList.remove('hidden');
+        setTimeout(() => {
+            navigationInner.classList.toggle('open');
+        }, 50);
+        expanderBtn.classList.toggle('bx-chevron-down');
+        expanderBtn.classList.toggle('bx-chevron-up');
+    } else {
+        navigationInner.classList.remove('open');
+        setTimeout(() => {
+            navigationInner.classList.add('hidden');
+        }, 350);
+        expanderBtn.classList.toggle('bx-chevron-down');
+        expanderBtn.classList.toggle('bx-chevron-up');
+    }
+
+});
